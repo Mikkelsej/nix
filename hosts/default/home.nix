b6d36a6895c1dee174edd4a99810c08655065e43
MikkelSej <Mikkel.sejdelin@gmail.com>
2025-03-27 10:25:32 +0100

    Small configs

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -80,5 +80,6 @@
     ./../../modules/home-manager/git.nix
     ./../../modules/home-manager/rofi.nix
     ./../../modules/home-manager/desktopenv/waybar.nix
+    ./../../modules/nixos/thunar.nix
   ];
 }
---
+++ hosts/default/variables.nix
@@ -0,0 +1,8 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "MikkelSej";
+  gitEmail = "mikkel.sejdelin@gmail.com";
+
+  # Enable Thunar GUI File Manager
+  thunarEnable = false;
+}
